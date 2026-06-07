d7d7ffcf1b82870f6d502605410a529d3896ab43
unknown <mathapationkar8@gmail.com>
2026-06-07 22:06:18 +0530

    disallow signup pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.alphanom.in/',
-  generateRobotsTxt: true, // Generates a robots.txt file alongside it
-  generateIndexSitemap: false, // Set to true if you have over 50,000 URLs
+  siteUrl: process.env.SITE_URL || 'https://alphanom.in',
+  generateRobotsTxt: true, 
+  generateIndexSitemap: false,
+  
+  // Add the routes you want to hide from Google here
+  exclude: [
+    '/auth/*',                 // Excludes all forgot-password and auth sub-routes
+    '/employer/login',         // Excludes employer login
+    '/employer/signup',        // Excludes employer signup
+    '/recruiter/login',        // Excludes recruiter login
+    '/recruiter/signup',       // Excludes recruiter signup
+  ],
+
+  // Optional: This ensures the robots.txt file blocks bots from crawling them too
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/auth/*',
+          '*/login',
+          '*/signup',
+        ],
+      },
+    ],
+  },
 }
